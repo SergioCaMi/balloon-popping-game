8aec5dc81f26fc0e9f22059451e0d8aab028ceff
SergioCaMi <sergiocami84@gmail.com>
2025-04-04 14:33:44 +0200

    fin ejercicio

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// tu código aquí
+document.querySelectorAll(".balloon").forEach((balloon) => {
+  balloon.addEventListener("mouseover", (e) => {
+    const remainingBalloons = document.querySelectorAll(".balloon");
+    console.log("---" + remainingBalloons.length);
+    if (remainingBalloons.length > 0) {
+      e.target.classList.remove("balloon");
+      e.target.style.backgroundColor = "#EDEDED";
+      e.target.textContent = "POP!";
+    } else {
+      document.getElementById("yay-no-balloons").style.display = "block";
+      document.getElementById("balloon-gallery").style.display = "none";
+    }
+  });
+});
